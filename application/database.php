a6c8e9b5b174146681585ed42ac261c056ba3931
7IN0SAN9 <me@7in0.me>
2016-09-05 17:50:06 +0800

    Init.

---
+++ application/database.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'type'     => 'mysql',
+    'hostname' => '127.0.0.1',
+    'database' => '',
+    'username' => 'root',
+    'password' => '',
+    'hostport' => '',
+    'dsn'      => '',
+    'charset'  => 'utf8',
+    'prefix'   => '',
+    'debug'    => true,
+];
